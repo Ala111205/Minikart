7b0b22f33f5ba858ce7f91ed07c700e6f4b6c2ba
Sadham Hussain <sadham070403@gmail.com>
2025-05-29 19:23:43 +0530

    g

--- e-commerce/front-end/src/Pages/Shop.jsx
+++ e-commerce/front-end/src/Pages/Shop.jsx
@@ -83,7 +83,7 @@ export default function Shop() {
     prod.brand?.toLowerCase().includes(searchTerm) ||
     prod.processor?.toLowerCase().includes(searchTerm)
   );
-  if(!filteredProducts){
+  if(!products){
     
     return <div className='loading'></div>;
   }
