cf335122708071e4b45a7fff4fe57396e44b33bc
Sadham Hussain <sadham070403@gmail.com>
2025-05-29 07:42:08 +0530

    bb

--- e-commerce/front-end/src/components/ProductForm.jsx
+++ e-commerce/front-end/src/components/ProductForm.jsx
@@ -71,8 +71,8 @@ export default function ProductForm({ onProductAdded, productToEdit, ClearEdit }
 
       const uploadRes = await axios.post("https://minikart-backend.onrender.com/api/products/upload", formData, {
         headers: {
-          "Content-Type":"multipart/form-data",
           Authorization: `Bearer ${token}`,
+          "Content-Type":"multipart/form-data",
         },
       });
 
