c9933c628d42576b01977655880ff8ae106f156a
Ala111205 <sadham070403@gmail.com>
2026-02-03 21:44:00 +0530

    type

--- e-commerce/back-end/models/Order.js
+++ e-commerce/back-end/models/Order.js
@@ -19,7 +19,6 @@ const orderSchema = new mongoose.Schema({
       product: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Product',
-        required: true
       },
 
       quantity: {
