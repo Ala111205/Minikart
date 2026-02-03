86006d4a683ddb76ea9e2edc9926e52487ae22de
Ala111205 <sadham070403@gmail.com>
2026-02-03 19:52:34 +0530

    console back

--- e-commerce/back-end/middleware/verifyUser.js
+++ e-commerce/back-end/middleware/verifyUser.js
@@ -2,10 +2,13 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
+  console.log("HEADER:", req.headers.authorization);
+
   if (!token) return res.status(401).json({ message: "No token" });
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
   req.user = decoded;
+  console.log("USER:", req.user);
   next();
 };
