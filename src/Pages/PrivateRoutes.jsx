27586ad1748df2bf4e72b17556e10b435ab826c2
Ala111205 <sadham070403@gmail.com>
2026-02-05 13:39:29 +0530

    remove old root vite app

--- src/Pages/PrivateRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-export default function PrivateRoute({ children }) {
-  const token = localStorage.getItem("adminToken");
-  if(!token){
-    return  <Navigate to="/" replace />;
-  }
-  return children;
-}
